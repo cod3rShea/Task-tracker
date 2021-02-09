f73283e29f1a47a3661dbcb3120bfe9e1aaa762b
Sean Michael Shea <20631582+cod3rShea@users.noreply.github.com>
2021-02-09 07:49:39 -0500

    create a add a task form component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 function App() {
 
@@ -24,6 +25,7 @@ function App() {
     setTasks( tasks.filter( (task) => task.id !== id) )
   }
 
+  // Toggle Reminder 
   const toggleReminder = (id) => {
     setTasks( 
       tasks.map((task) => 
@@ -36,6 +38,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask />
       {tasks.length > 0 ? (
         <Tasks 
           tasks={tasks} 
